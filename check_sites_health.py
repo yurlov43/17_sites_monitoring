83b57d45214c1cc415aab059e5725eed9b750e52
yurlov43 <yurlov43@yandex.ru>
2018-06-12 13:35:39 +0300

    The first implementation of the program

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,45 @@
-def load_urls4check(path):
-    pass
+import requests
+import argparse
+import whois
+from colorama import init, Fore
+
+
+def load_urls4check(filepath):
+    with open(filepath, "r") as text_file:
+        return text_file.read()
+
 
 def is_server_respond_with_200(url):
-    pass
+    response = requests.get(url)
+    return response.status_code
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def get_domain_expiration_date(url):
+    return whois.whois(url).expiration_date
+
+
+def print_the_result(url, status_code, expiration_date):
+    print('\nUrl: {}\nStatus code: {}\nExpiration date: {}'.format(
+        url, status_code, expiration_date))
+
+
+def print_the_error(url):
+    init()
+    print('\nUrl: {}\n{}Seems like dns lookup failed.{}'.format(
+        url, Fore.RED, Fore.RESET))
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'filepath',
+        help='The path to the image')
+    arguments = parser.parse_args()
+    url_list = load_urls4check(arguments.filepath).split('\n')
+    for url in url_list:
+        try:
+            status_code = is_server_respond_with_200(url)
+            expiration_date = get_domain_expiration_date(url)
+            print_the_result(url, status_code, expiration_date)
+        except requests.exceptions.ConnectionError:
+            print_the_error(url)
