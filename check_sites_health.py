d955515912b9cc316c6c29376bd0c0206ce7b279
yurlov43 <yurlov43@yandex.ru>
2018-06-12 21:51:56 +0300

    Added calculation of days before expiration

--- check_sites_health.py
+++ check_sites_health.py
@@ -2,6 +2,7 @@ import requests
 import argparse
 import whois
 from colorama import init, Fore
+from datetime import datetime
 
 
 def load_urls4check(filepath):
@@ -18,15 +19,14 @@ def get_domain_expiration_date(url):
     return whois.whois(url).expiration_date
 
 
-def print_the_result(url, status_code, expiration_date):
-    print('\nUrl: {}\nStatus code: {}\nExpiration date: {}'.format(
-        url, status_code, expiration_date))
+def print_the_result(status_code, expiration_date):
+    print('Status code: {}\nExpiration date: {}'.format(
+        status_code, expiration_date))
 
 
-def print_the_error(url):
+def print_the_error(error):
     init()
-    print('\nUrl: {}\n{}Seems like dns lookup failed.{}'.format(
-        url, Fore.RED, Fore.RESET))
+    print('{}{}{}'.format(Fore.RED, error, Fore.RESET))
 
 
 if __name__ == '__main__':
@@ -36,10 +36,20 @@ if __name__ == '__main__':
         help='The path to the image')
     arguments = parser.parse_args()
     url_list = load_urls4check(arguments.filepath).split('\n')
+    today = datetime.now()
     for url in url_list:
+        print('\n{}'.format(url))
         try:
             status_code = is_server_respond_with_200(url)
+            if status_code != 200:
+                print_the_error('Something went wrong.')
             expiration_date = get_domain_expiration_date(url)
-            print_the_result(url, status_code, expiration_date)
+            if type(expiration_date) is list:
+                expiration_date = expiration_date[0]
+            number_of_days = (expiration_date - today).days
+            if number_of_days <= 30:
+                print_the_error(
+                    'Less than a month left before the expiration date.')
+            print_the_result(status_code, expiration_date)
         except requests.exceptions.ConnectionError:
-            print_the_error(url)
+            print_the_error('Seems like dns lookup failed.')
