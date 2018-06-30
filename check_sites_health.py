899ec365d5a2d689f47202191df2983e4f0211d1
yurlov43 <yurlov43@yandex.ru>
2018-06-30 13:47:43 +0300

    Fixed function names for more suitable

--- check_sites_health.py
+++ check_sites_health.py
@@ -4,27 +4,40 @@ import whois
 from datetime import datetime
 
 
-def load_urls4check(filepath):
-    with open(filepath, "r") as text_file:
+def load_text(filepath):
+    with open(filepath, 'r') as text_file:
         return text_file.read()
 
 
-def check_is_server_respond_with_200(url):
+def get_server_response_with_ok(url):
     response = requests.get(url)
-    status_code = response.status_code
-    if status_code != 200:
-        return 'Something went wrong.'
-    return 'Good! Status code: 200'
+    return response.ok
 
 
-def check_domain_expiration_date(url):
+def get_domain_expiration_date(url):
     expiration_date = whois.whois(url).expiration_date
     if type(expiration_date) is list:
         expiration_date = expiration_date[0]
-    number_of_days = (expiration_date - today).days
-    if number_of_days <= 30:
-        return 'Less than a month left before the expiration date.'
-    return 'Good! Until the expiry date is more than a month.'
+    return expiration_date
+
+
+def calculate_reserve_days(expiration_date):
+    if expiration_date:
+        reserve_days = (expiration_date - today).days
+    else:
+        return None
+    return reserve_days
+
+
+def print_messages_to_console(server_respond_ok, reserve_days, number_of_days):
+    if server_respond_ok:
+        print('Good! The server is OK.')
+    else:
+        print('Something went wrong.')
+    if reserve_days and reserve_days <= number_of_days:
+        print('Less than a month left before the expiration date.')
+    else:
+        print('Good! Until the expiry date is more than a month.')
 
 
 if __name__ == '__main__':
@@ -33,12 +46,18 @@ if __name__ == '__main__':
         'filepath',
         help='The path to the image')
     arguments = parser.parse_args()
-    url_list = load_urls4check(arguments.filepath).split('\n')
+    url_list = load_text(arguments.filepath).split('\n')
     today = datetime.now()
+    number_of_days = 30
     for url in url_list:
         print('\nURL: {}'.format(url))
         try:
-            print(check_is_server_respond_with_200(url))
-            print(check_domain_expiration_date(url))
+            server_respond_ok = get_server_response_with_ok(url)
+            expiration_date = get_domain_expiration_date(url)
+            reserve_days = calculate_reserve_days(expiration_date)
+            print_messages_to_console(
+                server_respond_ok,
+                reserve_days,
+                number_of_days)
         except requests.exceptions.ConnectionError:
             print('Seems like dns lookup failed.')
